b20024bc1e01319e96e5e8806def2ad59a41787d
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-02-19 04:29:38 +0530

    feat: create ResultScreen to display NIC details

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/nic_controller.dart';
+import '../widgets/nic_input_field.dart';
 
+class InputScreen extends StatelessWidget {
+  InputScreen({super.key});
+  final NICController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("NIC Decoder")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            NICInputField(controller: controller),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: controller.decodeNIC,
+              child: const Text("Decode NIC"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/nic_controller.dart';
+import '../widgets/result_card.dart';
 
+class ResultScreen extends StatelessWidget {
+  ResultScreen({super.key});
+  final NICController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("NIC Details")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: controller.nicDetails.entries
+              .map((e) => ResultCard(title: e.key, value: e.value.toString()))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
