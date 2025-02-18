44fab1d05a871a278ac1930b3e7203a434dda0ca
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-02-19 04:25:20 +0530

    feat: create NICInputField widget for entering NIC number

--- lib/widgets/nic_input_field.dart
+++ lib/widgets/nic_input_field.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/nic_controller.dart';
 
+class NICInputField extends StatelessWidget {
+  final NICController controller;
+  const NICInputField({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (value) => controller.nicNumber.value = value,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: "Enter NIC Number",
+      ),
+    );
+  }
+}
