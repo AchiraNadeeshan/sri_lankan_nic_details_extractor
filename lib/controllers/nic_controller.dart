9fcce1f0d60b4cdebe3d9678616dc40f5cda8d67
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-02-19 04:20:05 +0530

    feat: implement NICController for handling NIC decoding and state management

--- lib/controllers/nic_controller.dart
+++ lib/controllers/nic_controller.dart
@@ -1,1 +1,23 @@
+import 'package:get/get.dart';
+import '../utils/nic_decoder.dart';
 
+class NICController extends GetxController {
+  var nicNumber = ''.obs;
+  var nicDetails = {}.obs;
+
+  void decodeNIC() {
+    if (nicNumber.value.isEmpty) {
+      Get.snackbar("Error", "Please enter a NIC number",
+          snackPosition: SnackPosition.BOTTOM);
+      return;
+    }
+    var result = NICDecoder.decode(nicNumber.value);
+    if (result == null) {
+      Get.snackbar("Invalid NIC", "Enter a valid NIC number",
+          snackPosition: SnackPosition.BOTTOM);
+      return;
+    }
+    nicDetails.value = result;
+    Get.toNamed('/result');
+  }
+}
