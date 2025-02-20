0221ccea5cf5fd732ec028c0b32fb95aed76065b
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-02-20 17:24:42 +0530

    feat: add developed by section, update padding, and include developer URL

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/nic_controller.dart';
@@ -12,9 +10,10 @@ class InputScreen extends StatelessWidget {
 
   // Function to launch URL
   void _launchURL() async {
-    const url = 'https://github.com/AchiraNadeeshan';
-    if (await canLaunch(url)) {
-      await launch(url);
+    final Uri url = Uri.parse('https://github.com/AchiraNadeeshan');
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
     } else {
       throw 'Could not launch $url';
     }
@@ -24,86 +23,97 @@ class InputScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF181825), // Custom background color
-      body: SingleChildScrollView(
-        // Added ScrollView to prevent overflow
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment:
-                MainAxisAlignment.center, // Center content vertically
-            children: [
-              const SizedBox(height: 150), // Adjusted top padding
-              const Align(
-                alignment: Alignment.topCenter,
-                child: Text(
-                  "Sri Lankan\nNIC Details Extractor",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFFA6E3A1),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 40), // Adjust spacing
-              NICInputField(controller: controller),
-              const SizedBox(
-                  height: 20), // Space between input field and buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center content vertically
                 children: [
-                  ElevatedButton(
-                    onPressed: controller.decodeNIC,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFA6E3A1), // Updated color
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+                  const SizedBox(height: 50), // Adjusted top padding
+                  const Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "Sri Lankan\nNIC Details Extractor",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFA6E3A1),
                       ),
                     ),
-                    child: const Text(
-                      "EXTRACT",
-                      style:
-                          TextStyle(color: Color(0xFF181825)), // Updated color
-                    ),
                   ),
-                  const SizedBox(width: 20), // Space between buttons
-                  ElevatedButton(
-                    onPressed: controller.resetNIC,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFA6E3A1), // Updated color
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+                  const SizedBox(height: 40), // Adjust spacing
+                  NICInputField(controller: controller),
+                  const SizedBox(
+                      height: 20), // Space between input field and buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: controller.decodeNIC,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFA6E3A1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        child: const Text(
+                          "EXTRACT",
+                          style: TextStyle(
+                            color: Color(0xFF181825),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                    child: const Text(
-                      "RESET",
-                      style:
-                          TextStyle(color: Color(0xFF181825)), // Updated color
-                    ),
+                      const SizedBox(width: 20), // Space between buttons
+                      ElevatedButton(
+                        onPressed: controller.resetNIC,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFA6E3A1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        child: const Text(
+                          "RESET",
+                          style: TextStyle(
+                            color: Color(0xFF181825),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                  const SizedBox(height: 40), // Adjusted bottom spacing
                 ],
               ),
-              const SizedBox(height: 40), // Adjusted bottom spacing
-              // Developed by section
-              const SizedBox(height: 20), // Space before the developer info
-              Text(
-                "Developed by",
-                style: TextStyle(color: const Color(0xFF9399B2), fontSize: 16),
-              ),
-              GestureDetector(
-                onTap: _launchURL, // Open GitHub profile when tapped
-                child: Text(
-                  "AchiraNadeeshan",
-                  style: TextStyle(
-                    color: const Color(0xFFA6E3A1),
-                    fontSize: 16,
-                    decoration: TextDecoration
-                        .underline, // Optional: Underline for the link
+            ),
+            // "Developed by" section at the bottom
+            Column(
+              children: [
+                const Text(
+                  "Developed by",
+                  style: TextStyle(color: Color(0xFF9399B2), fontSize: 16),
+                ),
+                GestureDetector(
+                  onTap: _launchURL, // Open GitHub profile when tapped
+                  child: const Text(
+                    "AchiraNadeeshan",
+                    style: TextStyle(
+                      color: Color(0xFFA6E3A1),
+                      fontSize: 16,
+                      decoration: TextDecoration
+                          .underline, // Optional: Underline for the link
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
+                const SizedBox(height: 20), // Space from the bottom
+              ],
+            ),
+          ],
         ),
       ),
     );
