e54d65fe2be9a87890a66d2df2ab8cf0cad40ebf
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-02-21 01:43:16 +0000

    Deploying to gh-pages from @ AchiraNadeeshan/sri_lankan_nic_details_extractor@6f67527a51c0fcf6f43c1a327e4f41c2832b8a5e 🚀

--- lib/bindings/nic_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/nic_controller.dart';
-
-class NICBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<NICController>(() => NICController());
-  }
-}
