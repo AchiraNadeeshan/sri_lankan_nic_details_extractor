c96f673564b0baad12ecbb5d591585a773a61a0c
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-02-28 13:28:53 +0530

    docs: add DartDoc comments to main.dart for documentation generation

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,17 @@ import 'screens/input_screen.dart';
 import 'screens/result_screen.dart';
 import 'bindings/nic_binding.dart';
 
+/// The entry point of the application.
 void main() {
   runApp(const NICApp());
 }
 
+/// The main application widget.
+///
+/// This class initializes the Flutter application and sets up routing,
+/// themes, and dependency bindings using GetX.
 class NICApp extends StatelessWidget {
+  /// Creates an instance of [NICApp].
   const NICApp({super.key});
 
   @override
@@ -20,11 +26,12 @@ class NICApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-      initialBinding: NICBinding(),
-      initialRoute: '/',
+      initialBinding: NICBinding(), // Binds dependencies using GetX
+      initialRoute: '/', // Sets the initial route of the app
       getPages: [
-        GetPage(name: '/', page: () => InputScreen()),
-        GetPage(name: '/result', page: () => ResultScreen()),
+        GetPage(name: '/', page: () => InputScreen()), // Home screen route
+        GetPage(
+            name: '/result', page: () => ResultScreen()), // Result screen route
       ],
     );
   }
