638ecc3128aa37bf95a72c76d63e368c2527a106
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-03-01 15:10:03 +0530

    fix(ui): add better alignments in result screen

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -18,55 +18,60 @@ class ResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF181825), // Custom background color
-      body: SingleChildScrollView(
-        // Enables scrolling for smaller screens
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              /// Title of the result screen.
-              Padding(
-                padding: const EdgeInsets.only(top: 50.0),
-                child: const Text(
-                  "NIC Details",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFFA6E3A1),
+      body: Center(
+        child: SingleChildScrollView(
+          // Enables scrolling for smaller screens
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Center content vertically
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // Center content horizontally
+              children: [
+                /// Title of the result screen.
+                Padding(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: const Text(
+                    "NIC Details",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFA6E3A1),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 40), // Spacing below the title
+                const SizedBox(height: 40), // Spacing below the title
 
-              /// List of result cards displaying NIC details.
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: controller.nicDetails.entries
-                    .map((e) =>
-                        ResultCard(title: e.key, value: e.value.toString()))
-                    .toList(),
-              ),
-              const SizedBox(height: 50), // Spacing before the button
+                /// List of result cards displaying NIC details.
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: controller.nicDetails.entries
+                      .map((e) =>
+                          ResultCard(title: e.key, value: e.value.toString()))
+                      .toList(),
+                ),
+                const SizedBox(height: 50), // Spacing before the button
 
-              /// Button to navigate back to the input screen.
-              ElevatedButton(
-                onPressed: () => Get.back(),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFA6E3A1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+                /// Button to navigate back to the input screen.
+                ElevatedButton(
+                  onPressed: () => Get.back(),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFA6E3A1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Text(
+                    'GO BACK',
+                    style: TextStyle(
+                      color: Color(0xFF181825),
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                child: const Text(
-                  'GO BACK',
-                  style: TextStyle(
-                    color: Color(0xFF181825),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
